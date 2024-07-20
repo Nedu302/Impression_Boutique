e111c6e104a96253774862da720660ec2596f321
Mr. Naveed <123412684+Nedu302@users.noreply.github.com>
2024-07-20 13:12:08 +0500

    Update index.js

--- Back_end/index.js
+++ Back_end/index.js
@@ -1,43 +1,50 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from "cors";
 import dressRoute from './route/dressRoute.js';
 import ShopDressRouter from './route/ShopDress.js';
+import cors from "cors";
 import router from "./route/UserRoute.js";
-import Contactrouter from "./route/Contact_us.js";
+import Contactrouter from "./route/Contact_us.js"
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 8000 || 3001;
-const URI = "mongodb+srv://naveedahmad6028:xw4lyjllo3hhGHGr@cluster0.rp3ffvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const PORT = process.env.PORT || 3001;
+const URI = process.env.MongoDBURL;
 
-// Log the MongoDB connection string for debugging
-console.log("MongoDB Connection String:", URI);
-
-// Connect to MongoDB using Mongoose
-mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000, // 30 seconds
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch(error => {
-    console.error("Error connecting to MongoDB:", error.message);
+const client = new MongoClient(URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-// Define routes
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+mongoose.connect(URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(error => console.log("Error: ", error));
+
 app.use("/", dressRoute);
 app.use("/components/ShopProd", ShopDressRouter);
 app.use("/components/", router);
 app.use("/components/", Contactrouter);
 
-// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on ${PORT}`);
 });
