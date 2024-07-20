cfb02ded850e872e4327258897dd5c76843c6d85
Mr. Naveed <123412684+Nedu302@users.noreply.github.com>
2024-07-20 16:35:13 +0500

    Update dressRoute.js

--- Back_end/route/dressRoute.js
+++ Back_end/route/dressRoute.js
@@ -2,6 +2,6 @@ import express from "express";
 import  getDress  from '../controller/DressController.js';
 
 const router=express.Router();
-router.get("https://impression-boutique.vercel.app/?categories=ambroidery,chiffon,rafiaData",getDress);
+router.get("?categories=ambroidery,chiffon,rafiaData",getDress);
 
 export default router;
