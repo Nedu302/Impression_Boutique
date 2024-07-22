7738075612ea3d19a2295cb057eeca473872dd43
Mr. Naveed <123412684+Nedu302@users.noreply.github.com>
2024-07-22 14:56:22 +0500

    Update index.js

--- Back_end/index.js
+++ Back_end/index.js
@@ -23,10 +23,13 @@ mongoose.connect(URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch(error => console.log("Error: ", error));
 
-app.use("/", dressRoute);
-app.use("/components/ShopProd", ShopDressRouter);
-app.use("/components/",router)
-app.use("/components/",Contactrouter);
+app.use("/", (req,res)=>
+    {
+        res.json("Ellow");
+    });
+//app.use("/components/ShopProd", ShopDressRouter);
+//app.use("/components/",router)
+//app.use("/components/",Contactrouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
