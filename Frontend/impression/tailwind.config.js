61816b0e01030bb975a088d4a3d9bd1248549a01
Mr. Naveed <123412684+Nedu302@users.noreply.github.com>
2024-07-23 12:44:41 +0500

    Update tailwind.config.js

--- Frontend/impression/tailwind.config.js
+++ Frontend/impression/tailwind.config.js
@@ -1,19 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html', // Adjust this line
-    './src/**/*.{js,ts,jsx,tsx}', // Ensure this matches your file structure
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       spacing: {
-        'wide': '0.5em', 
+        'wide': '0.5em',
         'wider': '1em',
+      },
+      screens: {
+
+        'md': '1000px',
       }
     },
-    fontFamily:{
-      abc:["Ubuntu Sans,sans-serif"],
-      new:["cormorantgaramond-light, cormorantgaramond,cormorant garamond, serif"],
+    fontFamily: {
+      abc: ["Ubuntu Sans,sans-serif"],
+      new: ["cormorantgaramond-light, cormorantgaramond,cormorant garamond, serif"],
       'custom-serif': ['"Times New Roman"', 'serif']
     },
     keyframes: {
@@ -23,26 +27,26 @@ module.exports = {
         '50%': { transform: 'translateX(1px)' },
         '75%': { transform: 'translateX(-1px)' },
       },
-      },
-      animation: {
-        zigzag: 'zigzag 0.5s ease-in-out infinite',
-      },
-    fontWeight:{
-      'hairline': 100,
-       'thin':100,
-       'font-bold':700
     },
-   
-    
+    animation: {
+      zigzag: 'zigzag 0.5s ease-in-out infinite',
+    },
+    fontWeight: {
+      'hairline': 100,
+      'thin': 100,
+      'font-bold': 700
+    },
+
+
   },
   extend: {
     width: {
-      'custom': '200px', 
-      'half-screen': '50vw', 
+      'custom': '200px',
+      'half-screen': '50vw',
     },
     height: {
-      'custom': '100px', 
-      'full-screen': '100vh', 
+      'custom': '100px',
+      'full-screen': '100vh',
     },
   },
   // plugins: [],
